bbc5681745c4e522207895f2d1351481cb11ad6d
Shrivardhan567 <130844866+Shrivardhan567@users.noreply.github.com>
2023-10-14 17:39:39 +0530

    Update index.js solution

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,112 +1,53 @@
 // Write your code here
 import {Component} from 'react'
-import DestinationItem from '../DestinationItem'
-import './index.css'
 
-const destinationsList = [
-  {
-    id: 1,
-    name: 'Melaka Mosque',
-    imgUrl: 'https://assets.ccbp.in/frontend/react-js/melaka-mosque-img.png',
-  },
-  {
-    id: 2,
-    name: 'Shrubland',
-    imgUrl: 'https://assets.ccbp.in/frontend/react-js/shrubland-img.png',
-  },
-  {
-    id: 3,
-    name: 'New York',
-    imgUrl: 'https://assets.ccbp.in/frontend/react-js/new-york-img.png',
-  },
-  {
-    id: 4,
-    name: 'Escarpment',
-    imgUrl: 'https://assets.ccbp.in/frontend/react-js/escarpment-img.png',
-  },
-  {
-    id: 5,
-    name: 'Westminster Abbey',
-    imgUrl:
-      'https://assets.ccbp.in/frontend/react-js/westminster-abbey-img.png',
-  },
-  {
-    id: 6,
-    name: 'South Downs National Park',
-    imgUrl:
-      'https://assets.ccbp.in/frontend/react-js/south-downs-national-park-img.png',
-  },
-  {
-    id: 7,
-    name: 'National Historic Site',
-    imgUrl:
-      'https://assets.ccbp.in/frontend/react-js/national-historic-site-img.png',
-  },
-  {
-    id: 8,
-    name: 'Tower Bridge',
-    imgUrl: 'https://assets.ccbp.in/frontend/react-js/tower-bridge-img.png',
-  },
-  {
-    id: 9,
-    name: 'Arc Here',
-    imgUrl: 'https://assets.ccbp.in/frontend/react-js/arc-here-img.png',
-  },
-  {
-    id: 10,
-    name: 'Steeple',
-    imgUrl: 'https://assets.ccbp.in/frontend/react-js/steeple-img.png',
-  },
-  {
-    id: 11,
-    name: 'Glaciokarst',
-    imgUrl: 'https://assets.ccbp.in/frontend/react-js/glaciokarst-img.png',
-  },
-  {
-    id: 12,
-    name: 'Parco Nazionale delle Cinque Terre',
-    imgUrl:
-      'https://assets.ccbp.in/frontend/react-js/parco-nazionale-delle-cinque-terre-img.png',
-  },
-]
+import DestinationItem from '../DestinationItem'
+
+import './index.css'
 
 class DestinationSearch extends Component {
   state = {
     searchInput: '',
-    destList: destinationsList,
   }
 
-  filteredDestinationList = event => {
-    this.setState({
-      searchInput: event.target.value,
-    })
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
   }
 
   render() {
-    const {searchInput, destList} = this.state
-    const destResults = destList.filter(eachDest =>
-      eachDest.name.toLowerCase().includes(searchInput),
+    const {searchInput} = this.state
+    const {destinationsList} = this.props
+    const searchResults = destinationsList.filter(eachDestination =>
+      eachDestination.name.toLowerCase().includes(searchInput.toLowerCase()),
     )
 
     return (
-      <div>
-        <h1>Destination Search</h1>
-        <input
-          type="search"
-          onChange={this.filteredDestinationList}
-          value={searchInput}
-        />
-        <label>
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
-            alt="search icon"
-          />{' '}
-        </label>
-        <ul>
-          {destResults.map(eachDest => (
-            <DestinationItem destDetails={eachDest} key={eachDest.id} />
-          ))}
-        </ul>
+      <div className="app-container">
+        <div className="destinations-search-container">
+          <h1 className="heading">Destination Search</h1>
+          <div className="search-input-container">
+            <input
+              type="search"
+              className="search-input"
+              placeholder="Search"
+              value={searchInput}
+              onChange={this.onChangeSearchInput}
+            />
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+              alt="search icon"
+              className="search-icon"
+            />
+          </div>
+          <ul className="destinations-list">
+            {searchResults.map(eachDestination => (
+              <DestinationItem
+                key={eachDestination.id}
+                destinationDetails={eachDestination}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
     )
   }
